f1ff3638578a3d6281c9868eea797f16072f5edc
Rie Takahashi <megumin@megu.dev>
2025-02-06 17:23:36 +0000

    feat: better library finding, update electron-hook to support flatpak --user

--- src/constants.rs
+++ src/constants.rs
@@ -2,7 +2,7 @@
 pub static LIBRARY: &str = "vencord_launcher";
 
 #[cfg(not(windows))]
-pub static LIBRARY: &str = "vencord_launcher.so";
+pub static LIBRARY: &str = "libvencord_launcher.so";
 
 pub static MOD_ENTRYPOINT: &str = "patcher.js";
 pub static RELEASE_URL: &str = "https://api.github.com/repos/vendicated/vencord/releases/latest";
@@ -24,3 +24,43 @@ pub static RELEASE_ASSETS: &[&str] = &[
     "renderer.css",
     "renderer.css.map",
 ];
+
+pub fn get_library_path() -> String {
+    // Get the executable dir
+    let executable_dir = std::env::current_exe().ok().and_then(|exe_path| {
+        exe_path
+            .parent()
+            .map(|parent_path| parent_path.to_path_buf())
+    });
+
+    // If the executable dir contains the library, return that path
+    if let Some(dir) = executable_dir {
+        let lib_path = dir.join(LIBRARY);
+        if lib_path.exists() {
+            return lib_path.to_str().unwrap().to_string();
+        }
+    }
+
+    // If the current dir contains the library, return that path
+    if let Ok(dir) = std::env::current_dir() {
+        let lib_path = dir.join(LIBRARY);
+        if lib_path.exists() {
+            return lib_path.to_str().unwrap().to_string();
+        }
+    }
+
+    // If neither paths contain the dir, hope it's in a registered libary path
+    LIBRARY.to_string()
+}
+
+pub fn asset_cache_dir() -> Option<std::path::PathBuf> {
+    let local_appdata = dirs::data_local_dir()?;
+
+    let dir = local_appdata.join("VencordLauncher").join("cache");
+
+    if !dir.exists() {
+        std::fs::create_dir_all(&dir).ok()?;
+    }
+
+    Some(dir)
+}
